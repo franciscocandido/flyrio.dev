666acfcba13f23c3ff522f4a041df7644630f04a
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-05-26 18:54:42 -0300

    feat(layouts): adds a base layout for the pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        syne: ["Syne"],
+        spaceGrotesk: ["SpaceGrotesk"],
+      },
+      colors: {
+        background: "#000000",
+        foreground: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
