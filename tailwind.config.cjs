f73bf35ddf561d141d5ed3f64ed0cdf460c13636
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-05-29 18:54:55 -0300

    chore(integrations): add and setup react integration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        syne: ["Syne"],
-        spaceGrotesk: ["SpaceGrotesk"],
+        syne: ['Syne'],
+        spaceGrotesk: ['SpaceGrotesk'],
       },
       colors: {
-        background: "#000000",
-        foreground: "#ffffff",
+        background: '#000000',
+        foreground: '#ffffff',
       },
     },
   },
