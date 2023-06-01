c693d78b2375c59d032e9c18f501723752a7e464
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-01 17:16:19 -0300

    feat(components): add a BaseSection component
    a component that serves as a base for the sections on the main page

---
+++ src/components/BaseSection.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode;
+  title: string;
+  id: string;
+}
+
+export default function BaseSection({ children, title, id }: Props) {
+  return (
+    <div
+      className="default-margin my-20 flex w-full flex-col items-center "
+      id={id}>
+      <h2 className="mb-14 text-5xl font-bold">{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
+}
