bd01ad8c357d05f6ff86aad56e43d5519ca23f95
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-09 16:16:48 -0300

    feat(content): create the content collection for projects

---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,9 @@
+import BaseSection from '@components/BaseSection';
+
+export default function ProjectsSection() {
+  return (
+    <BaseSection title="Portfólio" id="projects">
+      <h1>Portflio Section</h1>
+    </BaseSection>
+  );
+}
