dc4e2e904465e7d280acaaa088ff7b9655135f86
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-10 17:15:45 -0300

    refactor(components): adds a click to scroll to HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,9 @@
+import useScrollToElement from '@hooks/useScrollToElement';
 import { MdArrowDownward } from 'react-icons/md/index';
 
 export default function Hero() {
+  const scrollTo = useScrollToElement;
+
   return (
     <div className="default-margin flex h-screen flex-col items-center justify-center bg-topo-texture-mobile bg-cover shadow-[inset_0_0_150px_10px_#000000] sm:bg-topo-texture-desktop">
       <h1 className="font-title mt-auto text-center text-2xl font-bold md:text-5xl lg:text-5xl">
@@ -9,7 +12,12 @@ export default function Hero() {
         <br /> um desenvolvedor frontend do Brasil
       </h1>
 
-      <div className="mt-auto flex flex-col items-center gap-y-3 pb-20">
+      <div
+        onClick={() => {
+          scrollTo('about');
+        }}
+        title="Clique para Rolar"
+        className="mt-auto flex cursor-pointer flex-col items-center gap-y-3 pb-20">
         <span className="text-2xl font-bold">Scroll</span>
         <MdArrowDownward className="animate-bounce" size="50px" />
       </div>
