2c0a5975b93ccedf00c322db62a3f90a9bb22a76
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-13 20:06:22 -0300

    feat(components): wip: stylization and project listing

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,4 +1,5 @@
 import BaseSection from '@components/BaseSection';
+import ProjectCard from '@components/ProjectCard';
 import { getCollection } from 'astro:content';
 
 const allProjects = await getCollection('projects', ({ data }) => {
@@ -8,12 +9,21 @@ const allProjects = await getCollection('projects', ({ data }) => {
 export default function ProjectsSection(): JSX.Element {
   return (
     <BaseSection title="Portfólio" id="projects">
-      <h1>Portflio Section</h1>
-      {allProjects.map((projectEntry) => (
-        <>
-          <pre>{projectEntry.data.title}</pre>
-          <img src={projectEntry.data.image.src}></img>
-        </>
+      {allProjects.map(({ data }) => (
+        <div className="grid grid-cols-1 gap-10">
+          <ProjectCard
+            title={data.title}
+            description={data.description}
+            image={data.image}
+            tags={data.tags}
+          />
+          <ProjectCard
+            title={data.title}
+            description={data.description}
+            image={data.image}
+            tags={data.tags}
+          />
+        </div>
       ))}
     </BaseSection>
   );
