c8f64ffde9505ad6f5f368bf71fe6b293ebdafd5
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-23 17:16:01 -0300

    refactor(components): remove components useds for mdx

--- src/components/content/Paragraph.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
-
-export default function Paragraph({ children }: Props): JSX.Element {
-  return <p className="py-3">{children}</p>;
-}
