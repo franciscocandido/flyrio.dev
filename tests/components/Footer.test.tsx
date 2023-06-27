08e629e81415f8eef5f2a0141402bf0e0a2f6f5f
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-27 14:24:46 -0300

    test(components): add test for ProjectSectionCard component

--- tests/components/Footer.test.tsx
+++ tests/components/Footer.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react';
 
 import Footer from '@components/Footer';
 
-describe('Component: Footer', () => {
-  it('shoud match to snapshot', () => {
+describe('Footer', () => {
+  test('renders correctly', () => {
     const component = render(<Footer />);
     expect(component).toMatchSnapshot();
   });
 
-  it('should have the copyright notice text', () => {
+  test('should have the copyright notice text', () => {
     render(<Footer />);
 
     const currentYear = new Date().getFullYear();
