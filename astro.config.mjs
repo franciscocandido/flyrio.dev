7136e64ad15bbe0ce4a7d191bd6a24479cdc8b41
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-30 14:30:50 -0300

    chore(deps): add and setup astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,6 @@ export default defineConfig({
       },
     }),
     sitemap(),
+    robotsTxt(),
   ],
 });
