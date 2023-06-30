640142b5312cdd02d921ebb39a67860a14c7a5b4
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-30 14:27:19 -0300

    chore(deps): add and setup @atrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.flyrio.dev',
   integrations: [
     react(),
     tailwind({
@@ -12,5 +14,6 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    sitemap(),
   ],
 });
