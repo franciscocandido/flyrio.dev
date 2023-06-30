6a81890837f80a0299250dcb644d33a268e38124
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-06-29 21:11:59 -0300

    refactor(components): navbar is now fixed and gets an amazing background when scrolling

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,26 @@
 import NavbarLink from '@components/Navbar/NavbarLink';
+import useWindowScrollPosition from '@hooks/useWindowScrollPosition';
+import { useEffect, useState } from 'react';
 
 export default function Navbar(): JSX.Element {
+  const [isOnTop, setIsOnTop] = useState<boolean | null>(null);
+  const [{ y }] = useWindowScrollPosition();
+
+  useEffect(() => {
+    if (Number(y) <= 100) {
+      setIsOnTop(true);
+    } else {
+      setIsOnTop(false);
+    }
+  }, [y, isOnTop]);
+
   return (
-    <nav className="default-margin absolute flex w-full flex-row items-center justify-between">
+    <nav
+      className={`default-margin fixed z-10 flex w-full flex-row items-center justify-between border-b transition duration-300 ${
+        isOnTop
+          ? 'border-b-transparent bg-transparent'
+          : 'border-b-accent-2 bg-background bg-opacity-90 backdrop-blur'
+      }`}>
       <a href="/">
         <img
           className="w-10 transition hover:-translate-y-0.5"
