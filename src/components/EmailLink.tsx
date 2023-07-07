018aaf9c3f57e719819e034632ea4b9d89944e86
FranciscoCandido <franciscocandidolyrio@gmail.com>
2023-07-07 18:51:31 -0300

    refactor(components): add a highlighted border to EmailLink component

--- src/components/EmailLink.tsx
+++ src/components/EmailLink.tsx
@@ -18,7 +18,7 @@ export default function EmailLinkProps({
         onMouseEnter={() => setIsOnHover(true)}
         onMouseLeave={() => setIsOnHover(false)}
         href={`mailto:${email}`}
-        className="mt-10 min-w-[300px] self-center rounded border-2 border-shadow p-3 text-center font-spaceGrotesk font-bold text-paragraph transition hover:border-highlight hover:bg-highlight hover:text-foreground">
+        className="mt-10 min-w-[300px] self-center rounded border-2 border-highlight border-shadow p-3 text-center font-spaceGrotesk font-bold text-paragraph transition hover:border-highlight hover:bg-highlight hover:text-foreground">
         {hasCopied ? 'Copiado!' : isOnHover ? 'Clique para copiar.' : email}
       </a>
     </>
